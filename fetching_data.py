be33087ace5a83f988947ecdc54caab785e0522e
Soulaimane El Mennaoui <soulaimane.el-mennaoui@ensta-paris.fr>
2025-02-19 14:48:41 +0100

    starting a new project : hihi

---
+++ fetching_data.py
@@ -0,0 +1,21 @@
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def fetch_commodity_data(ticker, start='2020-01-01', end='2025-02-19'):
+    data = yf.download(
+        ticker,
+        start=start,
+        end=end,
+        progress=False,
+        auto_adjust=True
+    )
+    return data
+
+
+def fetch_multiple_commodities(commodity_dict, start='2020-01-01', end='2025-02-19'):
+    dict_commos = {}
+    for name, ticker in commodity_dict.items():
+        dict_commos[name] = fetch_commodity_data(ticker, start, end)
+    return dict_commos
